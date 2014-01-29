3d8a6aaae1d538abfee87db5a7e743187dde4990
Zifei Tong <soariez@gmail.com>
2014-01-29 23:44:29 +1100

    Handle instrument target in cmd_enable_event()
    Add instrument target data to the prototype of event_ust_enable_tracepoint().

--- src/bin/lttng-sessiond/cmd.c
+++ src/bin/lttng-sessiond/cmd.c
@@ -1477,7 +1477,7 @@ int cmd_enable_event(struct ltt_session *session, struct lttng_domain *domain,
 
 		/* At this point, the session and channel exist on the tracer */
 		ret = event_ust_enable_tracepoint(usess, uchan, event,
-				filter_expression, filter, exclusion);
+				filter_expression, filter, exclusion, target);
 		if (ret != LTTNG_OK) {
 			goto error;
 		}
