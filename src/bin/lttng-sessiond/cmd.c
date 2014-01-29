0dfc0a0a0308157e523ec5a63458842571a3ea4e
Zifei Tong <soariez@gmail.com>
2014-01-29 19:54:18 +1100

    Handle enable_event command with instrument target
    Define a structure holds the event instrument target data. The last
    data item is a flexible array that contains a variable number
    of target path.
    
    Add instrument target data to the prototype of cmd_enable_event(),
    and process the instrument target data form clients in the function.
    
    Add error codes of processing instrument traget data.

--- src/bin/lttng-sessiond/cmd.c
+++ src/bin/lttng-sessiond/cmd.c
@@ -1309,6 +1309,7 @@ int cmd_enable_event(struct ltt_session *session, struct lttng_domain *domain,
 		char *filter_expression,
 		struct lttng_filter_bytecode *filter,
 		struct lttng_event_exclusion *exclusion,
+		struct lttng_event_target *target,
 		int wpipe)
 {
 	int ret, channel_created = 0;
@@ -1459,7 +1460,7 @@ int cmd_enable_event(struct ltt_session *session, struct lttng_domain *domain,
 		tmp_dom.type = LTTNG_DOMAIN_UST;
 
 		ret = cmd_enable_event(session, &tmp_dom, DEFAULT_JUL_CHANNEL_NAME,
-			&uevent, filter_expression, filter, NULL, wpipe);
+			&uevent, filter_expression, filte, NULL, NULL, wpipe);
 		if (ret != LTTNG_OK && ret != LTTNG_ERR_UST_EVENT_ENABLED) {
 			goto error;
 		}
@@ -1679,7 +1680,7 @@ int cmd_enable_event_all(struct ltt_session *session,
 		tmp_dom.type = LTTNG_DOMAIN_UST;
 
 		ret = cmd_enable_event(session, &tmp_dom, DEFAULT_JUL_CHANNEL_NAME,
-			&uevent, NULL, NULL, NULL, wpipe);
+			&uevent, NULL, NULL, NULL, NULL, wpipe);
 		if (ret != LTTNG_OK && ret != LTTNG_ERR_UST_EVENT_ENABLED) {
 			goto error;
 		}
